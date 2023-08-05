fbdcdbbdd7906304278b69049a94814aefbf8391
R2boyo25 <r2boyo25@gmail.com>
2023-08-05 12:05:59 -0500

    Clean up mix

--- mix.exs
+++ mix.exs
@@ -7,10 +7,7 @@ defmodule Orgmode.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      compilers: [:leex] ++ [:yecc] ++ Mix.compilers(),
-      leex_options: [erlc_paths: ["lib"]],
-      yecc_options: [erlc_paths: ["lib"]]
+      deps: deps()
     ]
   end
 
--- test.exs
+++
@@ -1,5 +0,0 @@
-#IO.puts(inspect(Orgmode.parse("#+TITLE: Big wow, Much title!")))
-
-require Orgmode
-
-Orgmode.parse_file!("README.org")
